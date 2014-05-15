83984e9f5e09345ec2952dc1bc812a11d941446f
Daniel Sank <sank.daniel@gmail.com>
2014-05-15 08:02:18 -0700

    Added url and download_url

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(name='observed',
       description='Observer pattern for bound methods',
       author='Daniel Sank',
       author_email='sank.daniel@gmail.com',
+      url='https://github.com/DanielSank/observed',
+      download_url='https://github.com/DanielSank/observed/tarball/0.1',
       keywords=['observer', 'event', 'callback'],
       classifiers=[],
 )
