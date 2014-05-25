f75d3ac2e7025e8d0c3d747a17882f29b9f4aa82
Daniel Sank <sank.daniel@gmail.com>
2014-05-24 23:40:20 -0700

    Bumped version to 0.2.2 and fixed download URL

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(name='observed',
       packages = ['observed'],
-      version = '0.2',
+      version = '0.2.2',
       description='Observer pattern for bound methods',
       author='Daniel Sank',
       author_email='sank.daniel@gmail.com',
       url='https://github.com/DanielSank/observed',
-      download_url='https://github.com/DanielSank/observed/tarball/0.2',
+      download_url='https://github.com/DanielSank/observed/tarball/0.2.2',
       keywords=['observer', 'event', 'callback'],
       classifiers=[],
 )
