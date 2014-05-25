b16dd0bb8e6443253dba738493e4965835369a80
Daniel Sank <sank.daniel@gmail.com>
2014-05-25 08:47:21 -0700

    Version number now managed from observed/_version.py

--- observed/__init__.py
+++ observed/__init__.py
@@ -1,3 +1,4 @@
+from _version import __version__
 from observed.base import event
 
 __all__ = ['event']
--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
+# Get version information
+import re
+version_file = "observed/_version.py"
+verstrline = open(version_file, "rt").read()
+version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(version_re, verstrline, re.M)
+if mo:
+    version = mo.group(1)
+else:
+    msg = "Unable to find version string in %s." % (version_file,)
+    raise RuntimeError(msg)
+
 from distutils.core import setup
 
 setup(name='observed',
       packages = ['observed'],
-      version = '0.2.2',
+      version = version,
       description='Observer pattern for bound methods',
       author='Daniel Sank',
       author_email='sank.daniel@gmail.com',
       url='https://github.com/DanielSank/observed',
-      download_url='https://github.com/DanielSank/observed/tarball/0.2.2',
+      download_url='https://github.com/DanielSank/observed/tarball/'+version,
       keywords=['observer', 'event', 'callback'],
       classifiers=[],
 )
