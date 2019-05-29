95c4e831a83267ca2ef0b7f36257844ae913ea3e
Daniel Sank <sank.daniel@gmail.com>
2019-05-28 22:05:16 -0700

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ VERSION_RE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 
 
 # Get version information
-with open(VERSION_FILENAME, "rt") as version_file:
+with open(VERSION_FILENAME, "r") as version_file:
     mo = re.search(VERSION_RE, version_file.read(), re.M)
 
 if mo:
@@ -18,18 +18,18 @@ else:
     raise RuntimeError(msg)
 
 # Get description information
-with open(README_FILENAME, "rt") as description_file:
+with open(README_FILENAME, "r") as description_file:
     long_description = description_file.read()
 
 setuptools.setup(
-    name='observed',
+    name="observed",
     version=version,
-    author='Daniel Sank',
-    author_email='sank.daniel@gmail.com',
-    description='Observer pattern for functions and bound methods',
+    author="Daniel Sank",
+    author_email="sank.daniel@gmail.com",
+    description="Observer pattern for functions and bound methods",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/DanielSank/observed',
+    url="https://github.com/DanielSank/observed",
     py_modules=["observed"],
     classifiers=[
         "Programming Language :: Python :: 3",
